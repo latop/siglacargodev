e691291607d7c01f78b5dc5b1e3f2d075d4a5a95
Cristian <cristianhassfretes@gmail.com>
2023-09-20 09:09:38 -0400

    20/09

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"version.json": "a3d04c30ec5dacf9ff91f250f6a3b78c",
+const RESOURCES = {"version.json": "d12c26974e220610dbec2123da314d3c",
 "splash/splash.js": "c6a271349a0cd249bdb6d3c4d12f5dcf",
 "splash/style.css": "cf46f7f07ad5ec9ea98393b4bd4aa625",
-"index.html": "bf40671605aa206fc3a7b5d8bf75b632",
-"/": "bf40671605aa206fc3a7b5d8bf75b632",
-"main.dart.js": "bb392b2c14dbdb1dce9140cd97f7a242",
+"index.html": "baa607339fd266f1609b43832a255b32",
+"/": "baa607339fd266f1609b43832a255b32",
+"main.dart.js": "41f4666e82ac0d37d32e8aaf15a4a79f",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
